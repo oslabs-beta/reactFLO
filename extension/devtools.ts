3678aeee626c73b5776abbbdb8ce39c79b64e776
VaughnHartling <Voonhartking@gmail.com>
2020-05-06 11:58:29 -0700

    Migrated connection to background script from devtools.ts to index.ts.

--- extension/devtools.ts
+++ extension/devtools.ts
@@ -4,15 +4,3 @@ chrome.devtools.panels.create(
   'panel.html',
   // Not including function (watch trailing comma for errors)
 );
-
-let backgroundPageConnection = chrome.runtime.connect({
-  name: "panel"
-});
-
-backgroundPageConnection.onMessage.addListener(function(message){
-});
-
-backgroundPageConnection.postMessage({
-  name: 'init',
-  tabId: chrome.devtools.inspectedWindow.tabId,
-});
--- index.ts
+++ index.ts
@@ -2,3 +2,15 @@ const root = document.getElementById('root');
 const message = document.createElement('h1');
 message.innerText = 'Hello from index.ts';
 root.appendChild(message);
+
+let backgroundPageConnection = chrome.runtime.connect({
+  name: "panel"
+});
+
+backgroundPageConnection.onMessage.addListener(function(message){
+});
+
+backgroundPageConnection.postMessage({
+  name: 'init',
+  tabId: chrome.devtools.inspectedWindow.tabId,
+});
