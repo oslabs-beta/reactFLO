7c02e294707d4fe99d4f5aac542b68dba580bac9
Marcus Valverde <marcusvalverde18@gmail.com>
2020-05-07 11:45:36 -0700

    added initial hook file and boiler plate to patch from DevTools. Changed inject.js to inject.ts for webpack purposes

---
+++ extension/inject.ts
@@ -0,0 +1,15 @@
+import { initialHook } from './initialHook.ts';
+
+function messageToContentScript(message) {
+  window.postMessage(message, "*");
+};
+
+// Note: Index will add any text in the 'message' property to the dev tool dom
+messageToContentScript({
+  message: "message from injection site!"
+});
+
+// Feel free to remove when there is more code...
+console.log("hello from the injection site!");
+initialHook(); 
+
