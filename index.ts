52fa63bbe9e92ad28504e14ab5ead8a998e804c1
VaughnHartling <Voonhartking@gmail.com>
2020-05-06 18:40:03 -0700

    Added comments for readability.

--- index.ts
+++ index.ts
@@ -3,18 +3,21 @@ const message = document.createElement('h1');
 message.innerText = 'Hello from index.ts';
 root.appendChild(message);
 
+// Creates a connection to the background script
 let backgroundPageConnection = chrome.runtime.connect({
   name: "panel"
 });
 
-backgroundPageConnection.onMessage.addListener((msg) => {
-  
-  const newMessage = document.createElement('h2');
-  newMessage.innerText = msg.message;
-  root.appendChild(newMessage);
-});
-
+// Sends a message to the background script that initializes a connection to the dev tool page
 backgroundPageConnection.postMessage({
   name: 'init',
   tabId: chrome.devtools.inspectedWindow.tabId,
+});
+
+// Listens for a message from background script
+backgroundPageConnection.onMessage.addListener((msg) => {
+  // Test code: Renders messages from the background script to the dom
+  const newMessage = document.createElement('h2');
+  newMessage.innerText = msg.message;
+  root.appendChild(newMessage);
 });
