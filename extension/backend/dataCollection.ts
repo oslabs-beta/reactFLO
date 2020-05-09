0c78ce8096cab335c36dd419b43a7980cb0b444a
VaughnHartling <Voonhartking@gmail.com>
2020-05-09 15:01:23 -0700

    Props are now parsed.

--- extension/backend/dataCollection.ts
+++ extension/backend/dataCollection.ts
@@ -8,30 +8,62 @@ class SimpleNode implements DisplayNode {
   id: number;
   displayName: string;
   tag: number;
-  props: State[] = [];
-  state: State[] = [];
+  props: State[] | null = [];
+  state: State[] | null = [];
   children: DisplayNode[] = [];
   parent: string | null = null;
   constructor(node: any) {
     this.id = node._debugID;
     this.tag = node.tag;
+    this.state = convertState(node);
+    this.props = convertProps(node);
   }
 }
-  // STATE
-  // Check for hooks
-  // If memoizedState.memoizedState && _debugHookTypes = ['useState']
-  // Extract state labels from elementType - Need to convert function to string and extract
 
-  // Check for component state
-  // Check if tag === 1
-  // If so, memoizedState will be an object of key/value pairs of component state
+// STATE
+// Check for hooks
+// If memoizedState.memoizedState && _debugHookTypes = ['useState']
+// Extract state labels from elementType - Need to convert function to string and extract
 
-  // PROPS
-  // memoizedProps will be an object of key/value pairs of props
-  // We can also check tag to check for what type of component it is
+// Check for component state
+// Check if tag === 1
+// If so, memoizedState will be an object of key/value pairs of component state
+
+const convertState = (node) => {
+  // Create representations of state which can be queried
+  const state: State[] = [];
+  return state;
+}
+
+// PROPS
+// memoizedProps will be an object of key/value pairs of props
+// We can also check tag to check for what type of component it is
   
-const convertStateProps = (obj) => {
-  // Create representations of state and prop which can be queried
+const convertProps = (node) => {
+  // Check if node has props
+  // If not return null
+  if (!node.memoizedProps) return null;
+  // Create props array
+  const props: State[] = [];
+  // Iterate through memoizedProps.props
+  for (const key in node.memoizedProps) {
+    try {
+      // Create a prop object
+      const prop: State = {
+        // Store values in object
+        key,
+        value: node.memoizedProps[key] || null,
+        topComponent: null,
+        components: [],
+      };
+      // Push object to props array
+      props.push(prop);
+    } catch (error) {
+      continue;
+    }
+  }
+  // Return props array
+  return props;
 }
 
 const convertStructure = (node) => {
