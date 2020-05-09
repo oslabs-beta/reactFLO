03108af667d60bb89f958cd7df7e5a1e4b95b568
jdele <jordanscottdeleon@gmail.com>
2020-05-08 21:37:21 -0700

    added tag to interfaces

--- extension/backend/interfaces.ts
+++ extension/backend/interfaces.ts
@@ -1,9 +1,10 @@
-export interface FiberNode {
+export interface DisplayNode {
   id: number,
   displayName: string,
   props: State[] | null,
   state: State[] | null,
-  children: FiberNode[] | null,
+  children: DisplayNode[] | null,
+  tag: number,
 }
 
 export interface State {
