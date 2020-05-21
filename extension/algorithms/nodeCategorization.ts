14adc21d1a1e3c33b6539ce57f3f5c0794f758fc
VaughnHartling <Voonhartking@gmail.com>
2020-05-21 10:49:34 -0700

    Replaced findHeighestNode with createPathToRoot and workOnStatefulNodes.

--- extension/algorithms/nodeCategorization.ts
+++ extension/algorithms/nodeCategorization.ts
@@ -1,4 +1,5 @@
 import { DisplayNode, State } from "../backend/interfaces";
+import Traverse from "./dataTraversal";
 
 export default class FindProp {
   
@@ -46,3 +47,49 @@ export default class FindProp {
   }
 
 }
+
+// to create paths between startnode and the top stateful node 
+export const createPathToRoot = (startNode: DisplayNode): DisplayNode[] => {
+  // Create an array of stateful nodes
+  const statefulNodeArr: DisplayNode[] = [];
+  // Create an array of intermediary nodes, put startNode in med arr first because we dont want to count startNode as its own parent with same props 
+  let mediums: DisplayNode[] = [startNode];
+  // Traverse up the node tree
+  Traverse.upward(startNode.parent, (currentNode: DisplayNode) => {
+    // if it is stateful then push it to the stateful array 
+    if(currentNode.state) {
+      // push the current node so that we can find top of the chain 
+      statefulNodeArr.push(currentNode)
+      // push exisiting mediums we have kept into the top stateful node we found 
+      currentNode.mediums = [...mediums];
+      // reset mediums to include the current node mimicing what we did on 55  
+      mediums = [currentNode];
+    // else push to intermediary array 
+    } else {
+      mediums.push(currentNode);
+    }
+  });
+  // Return the array of stateful nodes
+  return statefulNodeArr;
+};
+
+// will change the display weight of all stateful component and path weights of all the mediums 
+export const workOnStatefulNodes = (nodes: DisplayNode[], prop: State): DisplayNode => {
+  // dec var for highest stateful node that has the props 
+  let highestNodeWithTarget: null | DisplayNode = null ; 
+  // iterate through the stateful nodes in reverse, from heighest to lowest
+  for(let i = nodes.length - 1; i >= 0; i--) {
+    // invoke FindProp.inState passing in statefulnode, this will see if the stateful node has the desired props 
+    FindProp.inState(nodes[i], prop);
+    // Check if the node has the prop and highestNodeWithTarget is null, if null assign highest to curr node 
+    if (nodes[i].displayWeight && !highestNodeWithTarget) highestNodeWithTarget = nodes[i];
+    // iterate through the mediums 
+    nodes[i].mediums.forEach((el) => {
+      // if display weight is 0 then set pathweight of medium to be equal to stateful node's path weight 
+      // Note: Replace first and last displayWeight with pathWeight for production
+      el.displayWeight = nodes[i].displayWeight || nodes[i].displayWeight;
+    });
+  }
+  // Return the heighest stateful node that contains the props or return null if there are no stateful nodes
+  return highestNodeWithTarget;
+};
