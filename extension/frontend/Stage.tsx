2e5fe0a699d5174a5eddb88bd590ddc96a02a577
cmzhong <cheriemzhong@gmail.com>
2020-05-20 20:31:18 -0700

    TypeScript refactor: Stage.tsx complete

--- extension/frontend/Stage.tsx
+++ extension/frontend/Stage.tsx
@@ -1,11 +1,18 @@
 import React, { useEffect, useRef, useState } from "react"
 
-const Context = React.createContext(null)
+const Context = React.createContext<object | null>(null)
 
-export function Stage({ width, height, children }) {
+interface StageProps {
+  width: string,
+  height: string,
+  children: object
+}
+export const Stage = ({ width, height, children }: StageProps) => {
+  
   const svgRef = useRef(null)
   const [svg, setSvg] = useState(null)
   useEffect(() => setSvg(svgRef.current), [])
+
   return (
     <svg ref={svgRef} width={width} height={height}>
       <Context.Provider value={svg}>{children}</Context.Provider>
