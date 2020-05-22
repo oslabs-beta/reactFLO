00a4f550183bc7334c8494f973074ff0c597a678
jdele <jordanscottdeleon@gmail.com>
2020-05-22 10:45:25 -0700

    Setup Enzyme to run by adding babel.config.js

--- __tests__/frontend/App.test.tsx
+++ __tests__/frontend/App.test.tsx
@@ -4,7 +4,22 @@ import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
 import renderer from 'react-test-renderer'
 
+// Newer Enzyme versions require an adapter to a particular version of React
+configure({ adapter: new Adapter() });
+
 import App from '../../extension/frontend/App'
 // App should render a left panel and right panel
 // LeftPanel should be given props of data, clickedNode, and selectNode
-// RightPanel clickedNode, selectProp, clearTree
+// RightPanel should be given props of clickedNode, selectProp, clearTree
+
+describe('React App unit tests', () => {
+  let wrapper;
+
+  beforeAll(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('Expect App component to have div with className of panelWrap', () => {
+    expect(wrapper.find('div').toHaveClass('panelWrap'));
+  });
+});
