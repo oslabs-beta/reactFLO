77853d962466678d1587ac8da8d28e15e39b6e96
jdele <jordanscottdeleon@gmail.com>
2020-05-22 12:25:41 -0700

    added LeftPanel and RightPanel render tests

--- __tests__/frontend/App.test.tsx
+++ __tests__/frontend/App.test.tsx
@@ -3,23 +3,40 @@ import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
 import renderer from 'react-test-renderer'
+// import * as chrome from "sinon-chrome";
+
+import App from '../../extension/frontend/App'
+import LeftPanel from '../../extension/frontend/LeftPanel'
+import RightPanel from '../../extension/frontend/RightPanel'
 
 // Newer Enzyme versions require an adapter to a particular version of React
 configure({ adapter: new Adapter() });
 
-import App from '../../extension/frontend/App'
+
 // App should render a left panel and right panel
 // LeftPanel should be given props of data, clickedNode, and selectNode
 // RightPanel should be given props of clickedNode, selectProp, clearTree
 
-describe('React App unit tests', () => {
+describe('App unit tests', () => {
   let wrapper;
+  const backgroundPageConnection = chrome.runtime.connect({
+    name: "panel"
+  });
 
   beforeAll(() => {
     wrapper = shallow(<App />);
   });
 
   it('Expect App component to have div with className of panelWrap', () => {
-    expect(wrapper.find('div').toHaveClass('panelWrap'));
+    expect(wrapper.find('div').at(1).hasClass('panelWrap')).toEqual(true);
   });
+
+  it('Expect App component to render one LeftPanel', () => {
+    expect(wrapper.find(LeftPanel)).toHaveLength(1);
+  })
+
+  it('Expect App component to render one RightPanel', () => {
+    expect(wrapper.find(RightPanel)).toHaveLength(1);
+  })
+
 });
