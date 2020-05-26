13d824883a9fd626cd28da90bb07589cc1f7209d
VaughnHartling <Voonhartking@gmail.com>
2020-05-26 16:25:27 -0700

    Added throttle to initialHook.

--- extension/backend/initialHook.ts
+++ extension/backend/initialHook.ts
@@ -10,14 +10,35 @@ const devTools = window.__REACT_DEVTOOLS_GLOBAL_HOOK__;
 const reactInstance = window.__REACT_DEVTOOLS_GLOBAL_HOOK__.renderers;
 const instance = reactInstance.get(1);
 
+let payload: object | null = null;
+let ready: boolean = true;
+const time: number = 250;
+
+const throttle = (fiberNode) => {
+  payload = fiberNode;
+  if (ready) send(fiberNode);
+};
+
+const send = (message) => {
+  window.postMessage({ message: fiberNodeToTree(message), id: 'ReactFLO' }, '*');
+  payload = null;
+  ready = false;
+  load();
+};
+
+const load = () => {
+  setTimeout(()=>{
+    ready = true;
+    if (payload) send(payload);
+  }, time);
+};
 
 export const initialHook = () => {
   if (instance && instance.version) {
     devTools.onCommitFiberRoot = (function (original) {
       return function (...args) {
         const fiberNode = args[1].current;
-        window.postMessage({ message: fiberNodeToTree(fiberNode), id: 'ReactFLO' }, '*');
-
+        throttle(fiberNode);
         return original(...args);
       };
     })(devTools.onCommitFiberRoot);
